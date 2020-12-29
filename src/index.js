f34135084b4eee58b6f2a7015c350b38b622937f
Maor Kamilyan <MaorKamilyan@gmail.com>
2020-12-29 15:27:54 +0200

    removed warnings

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ThemeProvider } from "styled-components"
 
 ReactDOM.render(
     <App />,
