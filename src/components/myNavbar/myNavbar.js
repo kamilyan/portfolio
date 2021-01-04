e3f062bb654872829c3cd7499ea6cdece28a2b30
Maor Kamilyan <MaorKamilyan@gmail.com>
2021-01-04 18:50:43 +0200

    added form contact

--- src/components/myNavbar/myNavbar.js
+++ src/components/myNavbar/myNavbar.js
@@ -1,36 +1,73 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import Logo from "../../assets/icons/Logo.png";
+import React from 'react'
+import { scroller } from 'react-scroll'
 
-import "./myNavbar.css";
+import { Navbar, Nav } from 'react-bootstrap'
+import Logo from '../../assets/icons/Logo.png'
+
+import './myNavbar.css'
 
 const MyNavbar = () => {
   return (
     <>
       <Navbar
-        fixed="top"
+        fixed='top'
         collapseOnSelect
-        expand="md"
-        bg="dark"
-        variant="dark"
-        className="animate-navbar nav-theme justify-content-between"
-      >
-        <Navbar.Brand href="#home">
-          <img className="logo" src={Logo} alt="Porfolio Logo" />
+        expand='md'
+        bg='dark'
+        variant='dark'
+        className='animate-navbar nav-theme justify-content-between'>
+        <Navbar.Brand href='#home'>
+          <img className='logo' src={Logo} alt='Porfolio Logo' />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='ml-auto'>
+            <Nav.Link
+              onClick={() =>
+                scroller.scrollTo('about', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 200,
+                })
+              }>
+              About
+            </Nav.Link>
+            <Nav.Link
+              onClick={() =>
+                scroller.scrollTo('skills', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 200,
+                })
+              }>
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              onClick={() =>
+                scroller.scrollTo('projects', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 200,
+                })
+              }>
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              onClick={() =>
+                scroller.scrollTo('contact', {
+                  smooth: true,
+                  offset: 100,
+                  duration: 500,
+                })
+              }>
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      )
     </>
-  );
-};
+  )
+}
 
-export default MyNavbar;
+export default MyNavbar
--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,52 +1,97 @@
-import React from "react";
-import { Jumbotron, Row, Col, Button } from "react-bootstrap";
-import "./contact.css";
-
-const Contact = () => {
-  return (
-    <div id="contact">
-      <h1 className="pt-3 font-details pb-3">CONTACT ME</h1>
-      <Jumbotron className="contact-jumbotron">
-        <Row>
-          <Col className="d-flex justify-content-center flex-wrap">
-            <div className="m-2">
-              <a
-                href="mailto:maorkamilyan@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button variant="outline-danger" title="maorkamilyan@gmail.com">
-                  Email Me
-                </Button>
-              </a>
-            </div>
-            <div className="m-2">
-              <a
-                href="https://www.linkedin.com/in/maor-kamilyan/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button variant="outline-primary" title="Visit my LinkenIn">
-                  LinkedIn
-                </Button>
-              </a>
-            </div>
-            <div className="m-2">
-              <a
-                href="https://github.com/kamilyan"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button variant="outline-dark" title="Other projects">
-                  GitHub
-                </Button>
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </Jumbotron>
-    </div>
-  );
-};
-
-export default Contact;
+import React, { Component } from 'react'
+import * as emailjs from 'emailjs-com'
+import { Button, Form } from 'react-bootstrap'
+import './contact.css'
+class Contact extends Component {
+  state = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    const { email, subject, message } = this.state
+    let templateParams = {
+      from_name: email,
+      to_name: 'maorkamilyan@gmail.com',
+      subject: subject,
+      message_html: message,
+    }
+    emailjs.send(
+      'gmail',
+      'template_j0h9wfw',
+      templateParams,
+      'user_tosK8p7cQptxXwvfVFCtN'
+    )
+    this.resetForm()
+  }
+  resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    })
+  }
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value })
+  }
+  render() {
+    return (
+      <div id='contact' className='contactForm'>
+        <h1 className='headding1'>Get in Touch</h1>
+        <Form onSubmit={this.handleSubmit.bind(this)}>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type='email'
+              name='email'
+              value={this.state.email}
+              className='text-primary'
+              onChange={this.handleChange.bind(this, 'email')}
+              placeholder='Enter email'
+            />
+          </Form.Group>
+          <Form.Group controlId='formBasicName'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type='text'
+              name='name'
+              value={this.state.name}
+              className='text-primary'
+              onChange={this.handleChange.bind(this, 'name')}
+              placeholder='Name'
+            />
+          </Form.Group>
+          <Form.Group controlId='formBasicSubject'>
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              type='text'
+              name='subject'
+              className='text-primary'
+              value={this.state.subject}
+              onChange={this.handleChange.bind(this, 'subject')}
+              placeholder='Subject'
+            />
+          </Form.Group>
+          <Form.Group controlId='formBasicMessage'>
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as='textarea'
+              rows={3}
+              name='message'
+              className='text-primary'
+              value={this.state.message}
+              onChange={this.handleChange.bind(this, 'message')}
+            />
+          </Form.Group>
+          <Button variant='primary' type='submit'>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    )
+  }
+}
+export default Contact
